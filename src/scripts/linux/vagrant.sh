a13d048888cab2a13db1529630e2f1e6a5c77db3
Basil Peace <grv87@yandex.ru>
2018-05-07 01:41:28 +0300

    chore: check server certificate during Vagrant SSH key installation
    To improve security

--- src/scripts/linux/vagrant.sh
+++ src/scripts/linux/vagrant.sh
@@ -24,9 +24,9 @@
 pubkey_url="https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub";
 mkdir -p $HOME/.ssh;
 if command -v wget >/dev/null 2>&1; then
-    wget --no-check-certificate "$pubkey_url" -O $HOME/.ssh/authorized_keys;
+    wget "$pubkey_url" -O $HOME/.ssh/authorized_keys;
 elif command -v curl >/dev/null 2>&1; then
-    curl --insecure --location "$pubkey_url" > $HOME/.ssh/authorized_keys;
+    curl --location "$pubkey_url" > $HOME/.ssh/authorized_keys;
 else
     echo "Cannot download vagrant public key";
     exit 1;
